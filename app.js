986eee4bac1572723e4c3498354fcc0119b858a7
Hope324 <98042460+Hope324@users.noreply.github.com>
2024-03-23 09:48:22 +0530

    Update app.js
    dadsas

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ config({path:"./configuration/config.env"});
 export const app=express();
 
 app.use(cors({
-    origin:'http://localhost:3000',
+    origin:'https://helperhubclient.vercel.app/',
     credentials:true,
 }));
 app.use(cookieParser());
